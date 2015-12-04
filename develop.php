3f2d66a9df60e69503040f1d27d975412dd80fd5
Chu Thuy Hien <yenchuchu95@gmail.com>
2015-12-04 15:06:49 +0700

    sửa conflict

--- develop.php
+++ develop.php
@@ -1,4 +1,23 @@
+
+<!DOCTYPE html>
+<html>
+<body>
 // this is develop.php
 <?php 
-echo "chỉnh sửa file trên nhánh hotfix";
+echo "chỉnh sửa file trên nhánh hotfix(develop)";
  ?>
+<?php
+echo "this is develop.php!";
+echo "<br>";
+$x = 5;
+$y = 10;
+echo $x; 
+echo "<br>";
+echo $y;
+echo "<br>";
+echo $x + $y;
+echo "chinh sua tren nhanh hotfix";
+?>
+
+</body>
+</html>
