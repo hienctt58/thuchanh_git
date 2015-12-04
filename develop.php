74d0fc131b9de87cc7e0c23f07544be04afab70b
Chu Thuy Hien <yenchuchu95@gmail.com>
2015-12-04 14:30:19 +0700

    edit develop.php

--- develop.php
+++ develop.php
@@ -1,1 +1,18 @@
-// this is develop.php
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo "this is develop.php!";
+echo "<br>"
+$x = 5;
+$y = 10;
+echo $x; 
+echo "<br>"
+echo $y;
+echo "<br>"
+echo $x + $y;
+?>
+
+</body>
+</html>
