ce29910a870b94fb188ff1dd3912b93a0a3034da
Chu Thuy Hien <yenchuchu95@gmail.com>
2015-12-04 14:39:34 +0700

    chỉnh sửa develop.php trên nhánh hotfix

--- develop.php
+++ develop.php
@@ -4,14 +4,15 @@
 
 <?php
 echo "this is develop.php!";
-echo "<br>"
+echo "<br>";
 $x = 5;
 $y = 10;
 echo $x; 
-echo "<br>"
+echo "<br>";
 echo $y;
-echo "<br>"
+echo "<br>";
 echo $x + $y;
+echo "chinh sua tren nhanh hotfix";
 ?>
 
 </body>
